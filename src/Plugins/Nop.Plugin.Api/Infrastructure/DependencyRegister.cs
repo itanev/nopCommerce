f3ea693803647bb29efa2aa1831317e4fe360a36
itanev <iliyan.tanev@gmail.com>
2016-03-02 11:28:24 +0200

    Tests for the CustomersController

--- src/Plugins/Nop.Plugin.Api/Infrastructure/DependencyRegister.cs
+++ src/Plugins/Nop.Plugin.Api/Infrastructure/DependencyRegister.cs
@@ -3,11 +3,14 @@ using Autofac.Core;
 using AutoMapper;
 using Nop.Core.Configuration;
 using Nop.Core.Data;
+using Nop.Core.Domain.Customers;
 using Nop.Core.Infrastructure;
 using Nop.Core.Infrastructure.DependencyManagement;
 using Nop.Data;
 using Nop.Plugin.Api.Data;
 using Nop.Plugin.Api.Domain;
+using Nop.Plugin.Api.DTOs.Customers;
+using Nop.Plugin.Api.Helpers;
 using Nop.Plugin.Api.Models;
 using Nop.Plugin.Api.MVC;
 using Nop.Plugin.Api.Services;
@@ -36,6 +39,10 @@ namespace Nop.Plugin.Api.Infrastructure
             Mapper.CreateMap<ApiSettings, ConfigurationModel>();
             Mapper.CreateMap<ConfigurationModel, ApiSettings>();
 
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Customer, OrderCustomerDto>();
+            Mapper.CreateMap<Customer, CustomerForShoppingCartItemDto>();
+
             Mapper.CreateMap<Client, ClientModel>();
             Mapper.CreateMap<ClientModel, Client>();
         }
@@ -43,6 +50,10 @@ namespace Nop.Plugin.Api.Infrastructure
         private void RegisterPluginServices(ContainerBuilder builder)
         {
             builder.RegisterType<ClientService>().As<IClientService>().InstancePerLifetimeScope();
+            builder.RegisterType<CustomerApiService>().As<ICustomerApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<StateProvinceApiService>().As<IStateProvinceApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<CountryApiService>().As<ICountryApiService>().InstancePerLifetimeScope();
+            builder.RegisterType<AuthorizationHelper>().As<IAuthorizationHelper>().InstancePerLifetimeScope();
         }
 
         public int Order { get; }
