2bd25557bdd016c619659cf217b7c562ba0d36b9
itanev <iliyan.tanev@gmail.com>
2016-03-02 11:59:17 +0200

    Skeleton Implementation for the customers endpoint

---
+++ src/Plugins/Nop.Plugin.Api/Models/CustomersParameters/BaseParametersModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Nop.Plugin.Api.Models.CustomersParameters
+{
+    public class BaseParametersModel
+    {
+        public BaseParametersModel()
+        {
+            Fields = string.Empty;
+        }
+
+        public string Fields { get; set; }
+    }
+}
